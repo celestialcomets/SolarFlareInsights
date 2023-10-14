c68a2ce4d3cd9faa4f7e5ee6fc19500d7175ef6b
Brayan Gutierrez <68131493+Brayan695@users.noreply.github.com>
2023-10-14 14:54:09 -0500

    Update module1.py

--- module1.py
+++ module1.py
@@ -27,4 +27,74 @@ df_2 = df[batch]
 
 print(df_2)
 
+# Batch 3
+batch = ((df['month'].isin([5, 6, 7, 8])) & (df['year'] == 2004))
 
+# Use the conditions to filter the DataFrame
+df_3 = df[batch]
+
+print(df_3)
+
+# Batch 4
+batch = ((df['month'].isin([7, 8, 9, 10])) & (df['year'] == 2004))
+
+# Use the conditions to filter the DataFrame
+df_4 = df[batch]
+
+print(df_4)
+
+# Batch 5
+batch = ((df['month'].isin([9, 10, 11, 12])) & (df['year'] == 2004))
+
+# Use the conditions to filter the DataFrame
+df_5 = df[batch]
+
+print(df_5)
+
+# Batch 6
+batch = ((df['month'].isin([11, 12])) & (df['year'] == 2004)) | ((df['month'].isin([1, 2])) & (df['year'] == 2005))
+
+# Use the conditions to filter the DataFrame
+df_6 = df[batch]
+
+print(df_6)
+
+# Batch 7
+batch = ((df['month'].isin([1, 2, 3, 4])) & (df['year'] == 2005))
+
+# Use the conditions to filter the DataFrame
+df_7 = df[batch]
+
+print(df_7)
+
+# Batch 8
+batch = ((df['month'].isin([3, 4, 5, 6])) & (df['year'] == 2005))
+
+# Use the conditions to filter the DataFrame
+df_8 = df[batch]
+
+print(df_8)
+
+# Batch 9
+batch = ((df['month'].isin([5, 6, 7, 8])) & (df['year'] == 2005))
+
+# Use the conditions to filter the DataFrame
+df_9 = df[batch]
+
+print(df_9)
+
+# Batch 10
+batch = ((df['month'].isin([7, 8, 9, 10])) & (df['year'] == 2005))
+
+# Use the conditions to filter the DataFrame
+df_10 = df[batch]
+
+print(df_10)
+
+# Batch 11
+batch = ((df['month'].isin([9, 10, 11, 12])) & (df['year'] == 2005))
+
+# Use the conditions to filter the DataFrame
+df_11 = df[batch]
+
+print(df_11)
