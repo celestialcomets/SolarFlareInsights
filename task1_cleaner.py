bb67a9265df8c8a3add858ddcd798cb4f0efb4f0
celestialcomets <katiedangto@gmail.com>
2023-10-21 14:51:07 -0500

    Update task1_cleaner.py

--- task1_cleaner.py
+++ task1_cleaner.py
@@ -3,6 +3,7 @@ import random
 
 from sklearn.preprocessing import StandardScaler
 
+scaler = StandardScaler()
 task1 = pd.read_csv('Solar_flare_RHESSI_2004_05.csv')
 
 attributes = ['duration.s', 'total.counts', 'energy.kev', 'x.pos.asec', 'y.pos.asec', 'month', 'year']
@@ -10,10 +11,48 @@ working_data = task1[attributes]
 
 # Batch 1
 batch = ((working_data['month'].isin([1, 2, 3, 4])) & (working_data['year'] == 2004))
-
 # Use the conditions to filter the DataFrame
 df_1 = working_data[batch]
 
+# Batch 2
+batch = ((working_data['month'].isin([3, 4, 5, 6])) & (working_data['year'] == 2004))
+df_2 = working_data[batch]
+
+# Batch 3
+batch = ((working_data['month'].isin([5, 6, 7, 8])) & (working_data['year'] == 2004))
+df_3 = working_data[batch]
+
+# Batch 4
+batch = ((working_data['month'].isin([7, 8, 9, 10])) & (working_data['year'] == 2004))
+df_4 = working_data[batch]
+
+# Batch 5
+batch = ((working_data['month'].isin([9, 10, 11, 12])) & (working_data['year'] == 2004))
+df_5 = working_data[batch]
+
+# Batch 6
+batch = ((working_data['month'].isin([11, 12])) & (working_data['year'] == 2004)) | ((working_data['month'].isin([1, 2])) & (working_data['year'] == 2005))
+df_6 = working_data[batch]
+
+# Batch 7
+batch = ((working_data['month'].isin([1, 2, 3, 4])) & (working_data['year'] == 2005))
+df_7 = working_data[batch]
+
+# Batch 8
+batch = ((working_data['month'].isin([3, 4, 5, 6])) & (working_data['year'] == 2005))
+df_8 = working_data[batch]
+
+# Batch 9
+batch = ((working_data['month'].isin([5, 6, 7, 8])) & (working_data['year'] == 2005))
+df_9 = working_data[batch]
+
+# Batch 10
+batch = ((working_data['month'].isin([7, 8, 9, 10])) & (working_data['year'] == 2005))
+df_10 = working_data[batch]
+
+# Batch 11
+batch = ((working_data['month'].isin([9, 10, 11, 12])) & (working_data['year'] == 2005))
+df_11 = working_data[batch]
 
 def fetch_intensity(x_value, y_value, radius, df):
     x_upper = x_value + radius
@@ -51,7 +90,7 @@ def fetch_intensity_recursive(df):
 
 final_intensity_list_batch_1 = fetch_intensity_recursive(df_1)
 
-print(final_intensity_list_batch_1)
+#print(final_intensity_list_batch_1)
 
 
 attributes = ['duration.s', 'total.counts', 'energy.kev.i', 'energy.kev.f', 'x.pos.asec', 'y.pos.asec', 'month', 'year']
@@ -59,12 +98,8 @@ working_data = task1[attributes]
 
 # Batch 1
 batch = ((working_data['month'].isin([1, 2, 3, 4])) & (working_data['year'] == 2004))
-
-# Use the conditions to filter the DataFrame
 df_1 = working_data[batch]
-
 df_1['energy.kev.mid'] = (df_1['energy.kev.i'] + df_1['energy.kev.f']) / 2
 
-scaler = StandardScaler()
 df_1[['energy.kev.mid', 'duration.s']] = scaler.fit_transform(df_1[['energy.kev.mid', 'duration.s']])
-print(df_1[['energy.kev.mid', 'duration.s']])
+print(df_1)
